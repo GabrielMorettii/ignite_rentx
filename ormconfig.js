a18ab2466c6522359e30b3c8568c518c834b2b68
GabrielMoretti <gabrielmorettisilva@gmail.com>
2022-04-04 19:51:05 -0300

    fix: ORM

--- ormconfig.js
+++ ormconfig.js
@@ -3,10 +3,14 @@ console.log('process.env.DATABASE_URL :>>', process.env.DATABASE_URL)
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
-  "migrations": [`./dist/shared/infra/typeorm/migrations/*.js`],
-  "entities": [`./dist/modules/**/infra/typeorm/entities/*.js`],
+  "migrations": [
+    "dist/shared/infra/typeorm/migrations/*.js"
+  ],
+  "entities": [
+    "dist/modules/**/infra/typeorm/entities/*.js"
+  ],
   "cli": {
-    "migrationsDir": `./src/shared/infra/typeorm/migrations/`
+    "migrationsDir": "src/shared/infra/typeorm/migrations/"
   },
   "ssl": {
     rejectUnauthorized: false
