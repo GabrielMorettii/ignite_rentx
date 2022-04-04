86f45ddb2b2cc80e7d632ea0840112dedefc78d2
GabrielMoretti <gabrielmorettisilva@gmail.com>
2022-04-04 18:07:39 -0300

    fix: Heroku

--- ormconfig.js
+++ ormconfig.js
@@ -3,10 +3,10 @@ console.log('process.env.DATABASE_URL :>>', process.env.DATABASE_URL)
 module.exports = {
   "type": "postgres",
   "url": process.env.DATABASE_URL,
-  "migrations": [`./${process.env.MIGRATION_ENV}/shared/infra/typeorm/migrations/*.js`],
-  "entities": [`./${process.env.MIGRATION_ENV}/modules/**/infra/typeorm/entities/*.js`],
+  "migrations": [`./dist/shared/infra/typeorm/migrations/*.js`],
+  "entities": [`./dist/modules/**/infra/typeorm/entities/*.js`],
   "cli": {
-    "migrationsDir": `./src/shared/infra/typeorm/migrations`
+    "migrationsDir": `./src/shared/infra/typeorm/migrations/`
   },
   "ssl": {
     rejectUnauthorized: false
